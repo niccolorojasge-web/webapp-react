51295e5e36348fb5edc2bbaf21b0a84874871134
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-02-24 17:16:51 +0100

    review card

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,29 +1,45 @@
-import { Link } from "react-router-dom"
+import { Link, redirect } from "react-router-dom"
 import ReviewCard from "../components/ReviewCard"
+import { useParams } from "react-router-dom";
 import axios from "axios"
 import { useEffect,useState } from "react"
+const endpoint = "http://localhost:3000/api/movie/"
 
 function MoviePage() {
-
+const {id}= useParams();
+//settiamo la var di stato  per il libro 
+const [movie, setMovie]= useState({})
+//funzione di render chiama rotta show
+const fetchMovie=()=>{
+    axios.get(endpoint + id)
+    .then (res=>{setMovie(res.data)})
+    .catch (err=>{
+        console.log(err)
+    })
+}
+//richiamo funzione di rendering 
+useEffect(fetchMovie,[])
+//funzione di rendering per il li stato dei libri 
+const renderReviews =()=>{
+    return movie.results?.map(result=>{
+        return <ReviewCard reviewProp={result} key={result.id}/>
+    })
+}
   return (
     <>
      <header id="book" className="border-bottom border-1 mb-3">
                 <div className="d-flex mb-3">
-                    <img src="" alt="titolo del libro" />
+                    <img src={movie.image} alt="titolo del libro" />
                 </div>
-                <h1>Titolo libro</h1>
-                <h3 className="text-muted"><i>By Nome autore</i></h3>
-                <p>lorem ipsm dolor sit amet</p>
+                <h1>{movie.title}</h1>
+                <h3 className="text-muted"><i>{movie.author}</i></h3>
+                <p>{movie.abstract}</p>
             </header>
             <section id="reviews">
                 <header className="d-flex justify-content-between align-items-center mb-4">
                     <h4>Our community reviews</h4>
                 </header>
-                <ReviewCard/>
-                <ReviewCard/>
-                <ReviewCard/>
-                <ReviewCard/>
-                <ReviewCard/>
+                  {renderReviews()}
             </section>
             <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
                 <Link className="btn btn-secondary" to="/">Back to home</Link>
